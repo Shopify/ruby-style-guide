f3df77fc02f6cfac69be423f720effba1f59ffe8
Rouzbeh Majidi <rouzbeh.majidi@shopify.com>
2021-03-23 15:15:18 -0400

    Passthrough rubocop.yml to eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ module.exports = function (eleventyConfig) {
   })
   eleventyConfig.setUseGitIgnore(false)
   eleventyConfig.addPassthroughCopy('CNAME')
+  eleventyConfig.addPassthroughCopy('rubocop.yml')
   eleventyConfig.addPassthroughCopy({
     'node_modules/@fortawesome/fontawesome-free/webfonts': 'fonts/fontawesome'
   })
