93645f79b0a325ea2749f10d7872310506b29134
Sam Bostock <sam.bostock@shopify.com>
2022-06-20 14:48:57 -0400

    Ignore unused variable warnings in diffy gem
    A fix has been submitted upstream and is awaiting review.
    This unblocks merging the dependency bump until then.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,9 @@ require "pry-byebug"
 
 module Warning
   def self.warn(message)
+    # Remove once https://github.com/samg/diffy/pull/118 is merged
+    return if message =~ %r{lib/diffy/diff.rb:\d*: warning: assigned but unused variable - (?:stderr|process_status)}
+
     raise message.to_s
   end
 end
